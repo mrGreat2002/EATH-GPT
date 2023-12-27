8606266687e666bb58291a27ff66545d6555abf1
surya narayan <suryanature02@gmail.com>
2023-12-28 00:03:08 +0530

    this is secod commit

--- eath/src/App.js
+++ eath/src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import axios from 'axios';
 
 function App() {
+  const [question, setQuestion] = useState('');
+  const [response, setResponse] = useState('');
+  const [error, setError] = useState('');
+
+  const askHistory = async () => {
+    setError(''); // Clear previous error
+    setResponse(''); // Clear previous response
+    try {
+      const result = await axios.post('http://localhost:5000/askHistory', { question });
+      // Assuming the response structure matches this path
+      setResponse(result.data.choices[0].message.content);
+    } catch (error) {
+      setError(`Error: ${error.response ? error.response.data.message : error.message}`);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Ask a Historian</h1>
+      <input 
+        type="text"
+        value={question}
+        onChange={(e) => setQuestion(e.target.value)}
+        placeholder="Enter your history question"
+      />
+      <button onClick={askHistory}>Ask</button>
+      {response && <p>Response: {response}</p>}
+      {error && <p>{error}</p>}
     </div>
   );
 }
--- server/server.js
+++ server/server.js
@@ -1,9 +1,38 @@
 const express = require('express');
+const axios = require('axios');
+const cors = require('cors');
+require('dotenv').config();
+
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+app.use(cors());
+app.use(express.json());
+
+app.post('/askHistory', async (req, res) => {
+    try {
+        const response = await axios.post('https://api.cohere.ai/generate', {
+            model: "command",
+            prompt: `You are a knowledgeable historian. ${req.body.question}`,
+            max_tokens: 50,
+            temperature: 0.5,
+        }, {
+            headers: {
+                'Authorization': `Bearer c1Y0uUcjGbsDUC1xBiy79w8haXYhE6f9P8Q02uY0`,
+                'Content-Type': 'application/json'
+            }
+        });
+
+        res.json(response.data);
+    } catch (error) {
+        console.error("Error while querying Cohere:", error);
+        res.status(500).json({
+            message: "Error while querying Cohere",
+            details: error.message
+        });
+    }
 });
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
